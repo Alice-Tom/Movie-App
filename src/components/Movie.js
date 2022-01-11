66e835e7e28e0f6ba0c857dee0c76245f1eb9adf
Alice Tom M <admachuve@gmail.com>
2022-01-12 02:26:50 +0300

    removed package.json.lock

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
 //Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 //Components
